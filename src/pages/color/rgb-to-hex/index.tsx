470bff98926c63c8f321201f7b7a5721176f5207
ly2011 <1518550424@qq.com>
2024-10-20 15:51:02 +0800

    feat: 完善颜色代码转换器(RGBToHexPage)

--- src/pages/color/rgb-to-hex/index.tsx
+++ src/pages/color/rgb-to-hex/index.tsx
@@ -2,31 +2,83 @@
  * 颜色代码转换
  */
 import { useState } from 'react';
-import { ColorPicker, Button, Input, Form, Row, Col, Divider, theme, Space, message } from 'antd';
+import { ColorPicker, Button, Input, Tag, Row, Col, Divider, Typography, theme, Space, message } from 'antd';
 import { DownOutlined } from '@ant-design/icons';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import PageContainer from 'components/page-container';
 import { usePresets, useStyle, getColorString, convertColor } from './utils';
-import type { ColorPickerProps, Color, ColorFormat } from './utils';
+import type { ColorPickerProps, Color, ColorFormat, ColorType } from './utils';
 
+interface ColorPresets {
+  color: string;
+  className?: string;
+  onClick?: (color: string) => void;
+}
+
+const { Text } = Typography;
 function RGBToHexPage() {
   const [messageApi, contextHolder] = message.useMessage();
   const { token } = theme.useToken();
   const presets = usePresets();
   const { styles } = useStyle({ test: true });
-  const [form] = Form.useForm();
   const [open, setOpen] = useState<boolean>(false);
-  const [colorPicker, setColorPicker] = useState<Color>(token.colorPrimary);
+  const [colorPicker] = useState<Color>(token.colorPrimary);
   const [colorInput, setColorInput] = useState<string>('');
   const [colorFormat, setColorFormat] = useState<ColorFormat>('rgb');
-  const [colorHex, setColorHex] = useState<string>('');
-  const [colorRgb, setColorRgb] = useState<string>('');
-  const [colorRgba, setColorRgba] = useState<string>('');
-  const [colorHsb, setColorHsb] = useState<string>('');
 
-  // const initialValues = {
-  //   colorPicker: token.colorPrimary,
-  // };
+  // 快速颜色选择预设值
+  const colorPresets: ColorPresets[] = [
+    {
+      color: token.colorPrimary,
+      className: styles.presetsItem,
+      onClick: (color: string) => colorPresetsChange(color),
+    },
+    {
+      color: token.colorSuccess,
+      className: styles.presetsItem,
+      onClick: (color: string) => colorPresetsChange(color),
+    },
+    {
+      color: token.colorWarning,
+      className: styles.presetsItem,
+      onClick: (color: string) => colorPresetsChange(color),
+    },
+    {
+      color: token.colorError,
+      className: styles.presetsItem,
+      onClick: (color: string) => colorPresetsChange(color),
+    },
+    {
+      color: token.colorTextBase,
+      className: styles.presetsItem,
+      onClick: (color: string) => colorPresetsChange(color),
+    },
+    {
+      color: token.yellow,
+      className: styles.presetsItem,
+      onClick: (color: string) => colorPresetsChange(color),
+    },
+    {
+      color: token.gold,
+      className: styles.presetsItem,
+      onClick: (color: string) => colorPresetsChange(color),
+    },
+    {
+      color: token.pink,
+      className: styles.presetsItem,
+      onClick: (color: string) => colorPresetsChange(color),
+    },
+    {
+      color: token.cyan,
+      className: styles.presetsItem,
+      onClick: (color: string) => colorPresetsChange(color),
+    },
+    {
+      color: token.purple,
+      className: styles.presetsItem,
+      onClick: (color: string) => colorPresetsChange(color),
+    },
+  ];
 
   const success = (message: string) => {
     messageApi.open({
@@ -40,17 +92,6 @@ function RGBToHexPage() {
       type: 'error',
       content: message || '复制失败',
     });
-  };
-
-  const convertAllTypeColor = (color: string) => {
-    const innerHexValue = convertColor(color, 'hex');
-    const innerRgbValue = convertColor(color, 'rgb');
-    const innerRgbaValue = convertColor(color, 'rgba');
-    const innerHsbValue = convertColor(color, 'hsb');
-    setColorHex(innerHexValue);
-    setColorRgb(innerRgbValue);
-    setColorRgba(innerRgbaValue);
-    setColorHsb(innerHsbValue);
   };
 
   const customPanelRender: ColorPickerProps['panelRender'] = (_, { components: { Picker, Presets } }) => (
@@ -67,15 +108,8 @@ function RGBToHexPage() {
 
   const onColorPickerChange = (value: Color) => {
     const colorString = getColorString(value, colorFormat);
-    // console.log(colorString, ' colorString ===>');
-    // form.setFieldValue('colorInput', colorString);
     setColorInput(colorString);
-    convertAllTypeColor(colorString);
   };
-
-  // const onFinish = (values: any) => {
-  //   console.log('Success:', values);
-  // };
 
   const onCopy = (text: string, result: boolean) => {
     if (result) {
@@ -89,7 +123,6 @@ function RGBToHexPage() {
     const onValueChange = (e: React.ChangeEvent<HTMLInputElement>) => {
       const newValue = e.target.value;
       setColorInput(newValue);
-      convertAllTypeColor(newValue);
     };
 
     return (
@@ -108,66 +141,35 @@ function RGBToHexPage() {
           panelRender={customPanelRender}
         />
         <Input value={colorInput} onChange={onValueChange} />
+        {colorInput ? <Button style={{ width: 60, backgroundColor: colorInput }} /> : null}
       </Space.Compact>
     );
   };
 
-  const colorInputRender = (color: ColorFormat | 'rgba') => {
-    const addonBeforeText = color?.toUpperCase();
-    let value = '';
-    if (color === 'hex') {
-      value = colorHex;
-    } else if (color === 'rgb') {
-      value = colorRgb;
-    } else if (color === 'rgba') {
-      value = colorRgba;
-    } else if (color === 'hsb') {
-      value = colorHsb;
-    }
-    // const onValueChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    //   const newValue = e.target.value;
-    //   if (color === 'hex') {
-    //     setColorHex(newValue)
-    //     const innerRgbValue = convertColor(newValue, 'rgb')
-    //     const innerRgbaValue = convertColor(newValue, 'rgba')
-    //     const innerHsbValue = convertColor(newValue, 'hsb')
-    //     setColorRgb(innerRgbValue)
-    //     setColorRgba(innerRgbaValue)
-    //     setColorHsb(innerHsbValue)
-    //   } else if (color === 'rgb') {
-    //     setColorRgb(newValue)
-    //     const innerHexValue = convertColor(newValue, 'hex')
-    //     const innerRgbaValue = convertColor(newValue, 'rgba')
-    //     const innerHsbValue = convertColor(newValue, 'hsb')
-    //     setColorHex(innerHexValue)
-    //     setColorRgba(innerRgbaValue)
-    //     setColorHsb(innerHsbValue)
-    //   } else if (color === 'rgba') {
-    //     setColorRgba(newValue)
-    //     const innerHexValue = convertColor(newValue, 'hex')
-    //     const innerRgbValue = convertColor(newValue, 'rgb')
-    //     const innerHsbValue = convertColor(newValue, 'hsb')
-    //     setColorHex(innerHexValue)
-    //     setColorRgb(innerRgbValue)
-    //     setColorHsb(innerHsbValue)
-    //   } else if (color === 'hsb') {
-    //     setColorHsb(newValue)
-    //     const innerHexValue = convertColor(newValue, 'hex')
-    //     const innerRgbValue = convertColor(newValue, 'rgb')
-    //     const innerRgbaValue = convertColor(newValue, 'rgba')
-    //     setColorHex(innerHexValue)
-    //     setColorRgb(innerRgbValue)
-    //     setColorRgba(innerRgbaValue)
-    //   }
-    // };
+  const colorPresetsChange = (color: string) => {
+    setColorInput(color);
+  };
+
+  const colorPresetsRender = () => {
+    return (
+      <Space.Compact block>
+        <Text className={styles.presetsLabel}>示例输入: &nbsp;&nbsp;</Text>
+        {colorPresets.map((preset) => (
+          <Tag key={preset.color} color={preset.color} className={preset.className} onClick={() => preset.onClick?.(preset.color)}>
+            {preset.color}
+          </Tag>
+        ))}
+      </Space.Compact>
+    );
+  };
+
+  const colorInputRender = (colorType: ColorType) => {
+    const addonBeforeText = colorType?.toUpperCase();
+    const value = colorInput ? convertColor(colorInput, colorType) : '';
     return (
       <Space.Compact block>
         <Button style={{ width: 60 }}>{addonBeforeText}</Button>
         <Input value={value} readOnly />
-        {/* <Input value={value} readOnly onChange={onValueChange} /> */}
-        {/* <Button color="primary" variant="outlined">
-          转换
-        </Button> */}
         <CopyToClipboard text={value} onCopy={onCopy}>
           <Button color="primary" variant="outlined">
             复制
@@ -182,29 +184,14 @@ function RGBToHexPage() {
       {contextHolder}
       <div className={styles.wrapper}>
         <Space direction="vertical" size="middle" style={{ display: 'flex' }}>
-          {/* <Form layout="inline" form={form} name="search" onFinish={onFinish} initialValues={initialValues}>
-            <Form.Item name="colorPicker" label="">
-              <ColorPicker
-                format={colorFormat}
-                presets={presets}
-                open={open}
-                onOpenChange={setOpen}
-                onChange={onColorPickerChange}
-                onFormatChange={setColorFormat}
-                showText={() => <DownOutlined rotate={open ? 180 : 0} style={{ color: 'rgba(0, 0, 0, 0.25)' }} />}
-                styles={{ popupOverlayInner: { width: 480 } }}
-                panelRender={customPanelRender}
-              />
-            </Form.Item>
-            <Form.Item name="colorInput" label="" style={{ flex: 1 }}>
-              <Input />
-            </Form.Item>
-          </Form> */}
           {colorSearchRender()}
+          {colorPresetsRender()}
           {colorInputRender('hex')}
           {colorInputRender('rgb')}
           {colorInputRender('rgba')}
-          {colorInputRender('hsb')}
+          {colorInputRender('hsv')}
+          {colorInputRender('hsl')}
+          {colorInputRender('cmyk')}
         </Space>
       </div>
     </PageContainer>
--- src/pages/color/rgb-to-hex/utils.tsx
+++ src/pages/color/rgb-to-hex/utils.tsx
@@ -7,6 +7,7 @@ import { TinyColor } from '@ctrl/tinycolor';
 type Presets = Required<ColorPickerProps>['presets'][number];
 type Color = GetProp<ColorPickerProps, 'value'>;
 type ColorFormat = ColorPickerProps['format'];
+type ColorType = 'hex' | 'rgb' | 'rgba' | 'hsl' | 'hsla' | 'hsv' | 'hsva' | 'cmyk';
 
 const genPresets = (presets = presetPalettes) => Object.entries(presets).map<Presets>(([label, colors]) => ({ label, colors }));
 const usePresets = () => {
@@ -15,13 +16,20 @@ const usePresets = () => {
   return presets;
 };
 
-const useStyle = createStyles(({ token, css, prefixCls }) => {
+const useStyle = createStyles(({ css }) => {
   const styles = {
     wrapper: css`
       padding: 0;
     `,
     colorInput: css`
       flex: 1 !import;
+    `,
+    presetsLabel: css`
+      line-height: 30px !important;
+    `,
+    presetsItem: css`
+      font-size: 14px !important;
+      line-height: 28px !important;
     `
   };
 
@@ -54,56 +62,24 @@ const getColorString = (color: Color, colorFormat?: ColorFormat): string => {
   return color as unknown as string;
 };
 
-// 辅助函数：检查是否为有效的十六进制颜色
-const isHexColor = (color: string): boolean => /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/.test(color);
-
-// 辅助函数：检查是否为有效的 RGB 颜色
-const isRgbColor = (color: string): boolean => {
-  const regex = /^rgb\(\s*(\d{1,3})\s*,\s*(\d{1,3})\s*,\s*(\d{1,3})\s*\)$/;
-  const match = color.match(regex);
-  if (!match) return false;
-
-  const [r, g, b] = match.slice(1).map(Number);
-  return r >= 0 && r <= 255 && g >= 0 && g <= 255 && b >= 0 && b <= 255;
-};
-
-// 辅助函数：检查是否为有效的 RGBA 颜色
-const isRgbaColor = (color: string): boolean => {
-  const regex = /^rgba\(\s*(\d{1,3})\s*,\s*(\d{1,3})\s*,\s*(\d{1,3})\s*,\s*(\d+(\.\d+)?)\s*\)$/;
-  const match = color.match(regex);
-  if (!match) return false;
-
-  const [r, g, b, a] = match.slice(1).map(Number);
-  return r >= 0 && r <= 255 && g >= 0 && g <= 255 && b >= 0 && b <= 255 && a >= 0 && a <= 1;
-};
-
-// 辅助函数：检查是否为有效的 HSB 颜色
-const isHsbColor = (color: string): boolean => {
-  const regex = /^hsb\(\s*(\d{1,3})\s*,\s*(\d{1,3})%\s*,\s*(\d{1,3})%\s*\)$/;
-  const match = color.match(regex);
-  if (!match) return false;
-
-  const [h, s, b] = match.slice(1).map(Number);
-  return h >= 0 && h <= 360 && s >= 0 && s <= 100 && b >= 0 && b <= 100;
-};
-
-
 // 主函数：根据颜色类型进行转换
-const convertColor = (color: string, targetFormat: ColorFormat | 'rgba'): string => {
+const convertColor = (color: string, targetFormat: ColorType): string => {
   const tmpColor = new TinyColor(color);
   const newColor = tmpColor.clone();
   if (newColor.isValid) {
     switch (targetFormat) {
       case 'hex':
-        return newColor.toHexString();
+        return newColor.toHexString().toUpperCase();
       case 'rgb':
         return newColor.toRgbString();
       case 'rgba':
-        // 设置透明度为1
-        newColor.setAlpha(1.00);
         return newColor.toRgbString();
-      case 'hsb':
+      case 'hsv':
         return newColor.toHsvString();
+      case 'hsl':
+        return newColor.toHslString();
+      case 'cmyk':
+        return newColor.toCmykString();
       default:
         return color;
     }
@@ -111,5 +87,5 @@ const convertColor = (color: string, targetFormat: ColorFormat | 'rgba'): string
   return ''
 };
 
-export { usePresets, useStyle, getColorString, convertColor, isHexColor, isRgbColor, isRgbaColor, isHsbColor };
-export type { ColorPickerProps, Color, ColorFormat };
+export { usePresets, useStyle, getColorString, convertColor };
+export type { ColorPickerProps, Color, ColorFormat, ColorType };
